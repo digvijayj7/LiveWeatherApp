eec9e0257f86882de9ea68d4b8c57a5190eef16e
digvijayj7 <digvijayjadhav500500@gmail.com>
2023-07-19 16:05:40 +0530

    Updates

---
+++ precache-manifest.5d41de3835c176db0427dc006509bb18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f131cf365d3ef9d2f746f5f44f001aa",
+    "url": "/LiveWeatherApp/index.html"
+  },
+  {
+    "revision": "eaf7f6e3e4e35303bec3",
+    "url": "/LiveWeatherApp/static/css/main.01e4a207.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/LiveWeatherApp/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LiveWeatherApp/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf7f6e3e4e35303bec3",
+    "url": "/LiveWeatherApp/static/js/main.0822375e.chunk.js"
+  },
+  {
+    "revision": "5b1a5eea82c1b0a85b7f",
+    "url": "/LiveWeatherApp/static/js/runtime-main.a2dcfa05.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/LiveWeatherApp/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "16790c8e712f13fbd0686bb9d057717e",
+    "url": "/LiveWeatherApp/static/media/background.16790c8e.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/LiveWeatherApp/static/media/city.e1698332.jpg"
+  }
+]);
